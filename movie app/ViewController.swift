7b7af776b0e154deab14c3d0e206ea58c073be13
Satrio Teleng Bagaskoro <satrio.teleng@sekolahmu.co.id>
2022-09-24 16:03:01 +0700

    Initial Commit

---
+++ movie app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  movie app
+//
+//  Created by Satrio Teleng Bagaskoro on 24/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
